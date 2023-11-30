11f776f95b50b525da3a6546564b9944c7096e1c
Khubaib Ahmed <Khubaibahmed567@gmail.com>
2023-11-30 15:01:28 +0500

    modify_model_box

--- src/widgets/CheckInBox.js
+++ src/widgets/CheckInBox.js
@@ -5,18 +5,18 @@ function CheckInBox() {
   return (
     <div>
        {/* The button to open modal */}
-<label for="my_modal_6" class="btn btn-primary">Checked In</label>
+<label for="my_modal_1" class="btn btn-primary">Checked In</label>
 
 {/* Put this part before </body> tag */}
-<input type="checkbox" id="my_modal_6" class="modal-toggle" />
+<input type="checkbox" id="my_modal_1" class="modal-toggle" />
 <div class="modal" role="dialog">
 <div class="modal-box">
   <h3 class="font-bold text-lg">Hello!</h3>
   <p class="py-4">This modal works with a hidden checkbox!</p>
-  <div class="modal-action">
-  <label for="my_modal_6" class="btn ">Cancel</label>
-    <label for="my_modal_6" class="btn btn-primary">Add</label>
-  </div>
+  {/* <div class="modal-action">
+  <label for="my_modal_1" class="btn ">Cancel</label>
+    <label for="my_modal_1" class="btn btn-primary">Add</label>
+  </div> */}
 </div>
 </div>
     </div>
