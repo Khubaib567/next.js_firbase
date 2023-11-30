1062d705df8b696d525c1d9ff9a16d92e0ffeb89
Khubaib Ahmed <Khubaibahmed567@gmail.com>
2023-11-30 07:32:41 +0500

    check_responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,28 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div class="hero min-h-screen" style={{backgroundImage: "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);"}}>
-  <div class="hero-overlay bg-opacity-60"></div>
-  <div class="hero-content text-center text-neutral-content">
-    <div class="max-w-md">
-      <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-      <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button class="btn btn-primary">Get Started</button>
-    </div>
+    <>
+    <div class="carousel w-full">
+  <div id="item1" class="carousel-item w-full">
+    <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" class="w-full" />
+  </div> 
+  <div id="item2" class="carousel-item w-full">
+    <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" class="w-full" />
+  </div> 
+  <div id="item3" class="carousel-item w-full">
+    <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" class="w-full" />
+  </div> 
+  <div id="item4" class="carousel-item w-full">
+    <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" class="w-full" />
   </div>
+</div> 
+<div class="flex justify-center w-full py-2 gap-2">
+  <a href="#item1" class="btn btn-xs">1</a> 
+  <a href="#item2" class="btn btn-xs">2</a> 
+  <a href="#item3" class="btn btn-xs">3</a> 
+  <a href="#item4" class="btn btn-xs">4</a>
 </div>
+    </>
+   
   )
 }
